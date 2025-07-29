6cf6d5ad9012069792c576e087ef042c3995265d
Pulavarthi Satya Avanish <146957189+satyaavanish@users.noreply.github.com>
2025-07-29 12:03:37 +0530

    Create AboutAuthor.js

---
+++ src/AboutAuthor.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function AboutAuthor() {
+  return (
+    <div className="info-page">
+      <h1>About Author</h1>
+      <div className="info-content">
+        <p>
+          This application was created by [Your Name], a food enthusiast and developer passionate 
+          about combining technology with nutrition. The goal is to help people make better 
+          food choices through AI-powered suggestions and information.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutAuthor;
