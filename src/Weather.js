82ff829898c52adeec7561e70a99926c2c6f0b0b
Pulavarthi Satya Avanish <146957189+satyaavanish@users.noreply.github.com>
2025-09-28 22:41:23 +0530

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -249,7 +249,7 @@ function WeatherFoodSuggestions() {
 
     try {
       const response = await axios.post(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${GEMINI_API_KEY}`,
+         `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`,
         {
           contents: [{
             parts: [{
